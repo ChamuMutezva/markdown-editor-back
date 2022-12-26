6d92a63ba51279503adb8d9cdc4ad3b9a165713f
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-12-26 19:06:37 +0200

    routes adjusted

--- routes/editor.js
+++ routes/editor.js
@@ -12,18 +12,19 @@ const {
 
 
 // GET all markdown files
-router.get('/api/editor', getAllMarkdownFiles)
+router.get('/', getAllMarkdownFiles)
 
 //GET single markdown file
-router.get('/api/editor/:id', singleMarkdownFile)
+router.get('/:id', singleMarkdownFile)
 
 //POST a new file
-router.post('/api/editor', createMarkdown)
+router.post('/', createMarkdown)
 
 //DELETE A single markdown file
-router.delete('/api/editor/:id', deleteMarkdownFile)
+router.delete('/:id', deleteMarkdownFile)
 
 //UPDATE A maradown file
-router.patch('/api/editor/:id', updateMarkdownFile)
+// router.patch('/api/editor/:id', updateMarkdownFile)
+router.patch('/:id', updateMarkdownFile)
 
 module.exports = router
